537067f222b9bf2d8a4f834b7173dc56cd4bb7cb
Nishidha Panpaliya <npanpa23@in.ibm.com>
2020-11-30 19:22:43 +0000

    Fixed python interpreter not found error

--- buildscripts/set_python_path_for_bazelrc.sh
+++ buildscripts/set_python_path_for_bazelrc.sh
@@ -13,7 +13,9 @@ set -ex
 # Set python path variables from conda build environment
 BAZEL_RC_DIR=$1
 cat > $BAZEL_RC_DIR/python_configure.bazelrc << EOF
+build --incompatible_use_python_toolchains=false
 build --action_env PYTHON_BIN_PATH="$PYTHON"
 build --action_env PYTHON_LIB_PATH="$SP_DIR"
+build --action_env PATH="$PREFIX/bin:$PATH"
 build --python_path="$PYTHON"
 EOF
