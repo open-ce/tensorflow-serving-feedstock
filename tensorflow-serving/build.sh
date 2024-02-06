dd740fb82459794eefa379865060d29df2e52aa3
Aman Surkar <99606590+Aman-Surkar@users.noreply.github.com>
2024-02-06 17:34:31 +0530

    Suppressed Wno-array-bounds and Wno-stringop-overflow flags (#59)

--- tensorflow-serving/build.sh
+++ tensorflow-serving/build.sh
@@ -26,13 +26,13 @@ ARCH=`uname -p`
 
 if [[ $ppc_arch == "p10" ]]
 then
-    if [[ -z "${GCC_11_HOME}" ]];
+    if [[ -z "${GCC_HOME}" ]];
     then
-        echo "Please set GCC_11_HOME to the install path of gcc-toolset-11"
+        echo "Please set GCC_HOME to the install path of gcc-toolset-12"
         exit 1
     else
-        export CC=$GCC_11_HOME/bin/gcc
-        export CXX=$GCC_11_HOME/bin/g++
+        export CC=$GCC_HOME/bin/gcc
+        export CXX=$GCC_HOME/bin/g++
         export BAZEL_LINKLIBS=-l%:libstdc++.a
     fi
 else
